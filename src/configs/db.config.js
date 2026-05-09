e1517bad8ef589263e98b151a9113b058e041327
Arijit Ganguly <aganguly158@gmail.com>
2026-05-09 22:33:22 +0530

    #feat: connected the server to the db successfully

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -4,10 +4,18 @@ dotenv.config();
 
 const config = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username: process.env.DEV_DB_USER,
+        password: process.env.DEV_DB_PASSWORD,
+        database: process.env.DEV_DB_NAME,
+        host: process.env.DEV_DB_HOST,
+        dialect: 'mysql',
+    },
+
+    production: {
+        username: process.env.PROD_DB_USER,
+        password: process.env.PROD_DB_PASSWORD,
+        database: process.env.PROD_DB_NAME,
+        host: process.env.PROD_DB_HOST,
         dialect: 'mysql',
     }
 };
