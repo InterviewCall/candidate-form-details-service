16cbf4beec690d5a5dbda71e2156bd100cb5203b
Arnab <arnab3339@gmail.com>
2026-09-08 23:49:16 +0530

    Fixed Repository

--- src/repositories/CandidateSubmission.repository.ts
+++ src/repositories/CandidateSubmission.repository.ts
@@ -11,6 +11,16 @@ class CandidateSubmissionRepository extends BaseRepository<CandidateSubmission> 
     async create(data: CreationAttributes<CandidateSubmission>, transaction?: Transaction): Promise<CandidateSubmission> {
         return await this.model.create(data, { transaction });
     }
+    async findById(id: string): Promise<CandidateSubmission | null> {
+        const record = await this.model.findOne({
+            where: {
+                publicId: id
+            }
+        });
+
+        return record;
+    }
+
 
     async markSubmissionAsCompleted(id: string, data: Partial<InferAttributes<CandidateSubmission>>, transaction: Transaction): Promise<void> {
         await this.model.update(
@@ -22,7 +32,7 @@ class CandidateSubmissionRepository extends BaseRepository<CandidateSubmission> 
             },
             {
                 where: {
-                    id
+                    publicId: id
                 },
                 transaction
             },
