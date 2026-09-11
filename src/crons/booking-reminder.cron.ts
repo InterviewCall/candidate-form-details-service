cace6da01e9061e59507ed3223d7e0976b93ae49
Arijit Ganguly <aganguly158@gmail.com>
2026-09-11 12:29:32 +0530

    #feat: added queue producer to send booking reminder notifications

---
+++ src/crons/booking-reminder.cron.ts
@@ -0,0 +1,38 @@
+import cron from 'node-cron';
+
+import { addBookingReminderDetailsToQueue } from '../producers/reminderNotification.producer';
+import CandidateSubmissionRepository from '../repositories/CandidateSubmission.repository';
+import CandidateSubmissionService from '../services/CandidateSubmission.service';
+import { NotificationChannel } from '../utils/enums/NotificationChannel.enum';
+
+const candidateSubmissionService = new CandidateSubmissionService(
+    new CandidateSubmissionRepository()
+);
+
+export function bookingReminderCron(): void {
+    cron.schedule('*/2 * * * * ', async () => {
+        const pendingBookings = await candidateSubmissionService.findAllCandidatesWhereBookingPending();
+
+        if(pendingBookings.length > 0) {
+            for(const submission of pendingBookings) {
+                if(!submission.candidate) {
+                    continue;
+                }
+                await addBookingReminderDetailsToQueue({
+                    submissionId: submission.publicId,
+                    candidateId: submission.candidateId,
+                    candidateName: submission.candidate.fullName,
+                    candidateEmail: submission.candidate.email,
+                    candidatePhone: submission.candidate.phone,
+                    subject: 'Your InterviewCall booking is still pending',
+                    channels: [NotificationChannel.EMAIL],
+                    bookingLink: `http://localhost:3002/readiness/${submission.formSlug}/book-strategy-call?submission-id=${submission.publicId}`,
+                    templateKeys: {
+                        EMAIL: 'BookingReminder',
+                        WHATSAPP: 'BookingReminder'
+                    }
+                });
+            }
+        }
+    });
+}
