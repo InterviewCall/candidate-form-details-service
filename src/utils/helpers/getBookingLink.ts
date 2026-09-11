8e172dc5540b7a058844f947d0dfa7238560732c
Madhuboni Bhattacharya <madhubonibhattacharya03@gmail.com>
2026-09-11 18:09:15 +0530

    URL valiable name corrected

--- src/utils/helpers/getBookingLink.ts
+++ src/utils/helpers/getBookingLink.ts
@@ -1,8 +1,8 @@
 export function getBookingLink(formSlug: string, submissionId: string): string {
-    const baseUrl = process.env.BOOKING_FRONTEND_URL;
+    const baseUrl = process.env.CANDIDATE_FRONTEND_URL;
 
     if (!baseUrl) {
-        throw new Error('BOOKING_FRONTEND_URL is not configured');
+        throw new Error('CANDIDATE_FRONTEND_URL is not configured');
     }
 
     return `${baseUrl}/readiness/${formSlug}/book-strategy-call?submission-id=${submissionId}`;
